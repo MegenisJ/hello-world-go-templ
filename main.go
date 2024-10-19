2a91a6068ce3474fe71cbe56ec45ad5c5b788e63
JMegenis <james@megenis.com>
2024-10-19 22:12:19 +0100

    add pages directory

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
-
 	"github.com/a-h/templ"
+    "github.com/MegenisJ/hello-world-go-templ/Pages"
 )
 
 func main() {
-    component := hello("james")
+    component := pages.index()
     http.Handle("/", templ.Handler(component))
     fmt.Println("listening to http://localhost:3000")
     http.ListenAndServe(":3000", nil)
