c2f62fa13d88122d0d6d63ee2320da10b978afba
JMegenis <james@megenis.com>
2024-10-20 17:45:24 +0100

    added component

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
+	components "megenisj/hello-world/Components"
+	pages "megenisj/hello-world/pages"
 	"net/http"
+
 	"github.com/a-h/templ"
-    pages "megenisj/hello-world/pages"
 )
 
 func main() {
     component := pages.Index()
     http.Handle("/", templ.Handler(component))
+    button := components.Button()
+    http.Handle("/clicked", templ.Handler(button))
     fmt.Println("listening to http://localhost:3000")
     http.ListenAndServe(":3000", nil)
 }
