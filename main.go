7cc716876d370b2cb95a09dcab0854ffd14007b0
JMegenis <james@megenis.com>
2024-10-20 17:51:15 +0100

    spacing

--- main.go
+++ main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"github.com/a-h/templ"
 	components "megenisj/hello-world/Components"
 	pages "megenisj/hello-world/pages"
-	"net/http"
-
-	"github.com/a-h/templ"
 )
 
 func main() {
     component := pages.Index()
     http.Handle("/", templ.Handler(component))
+
     button := components.Button()
     http.Handle("/clicked", templ.Handler(button))
+
     fmt.Println("listening to http://localhost:3000")
     http.ListenAndServe(":3000", nil)
 }
